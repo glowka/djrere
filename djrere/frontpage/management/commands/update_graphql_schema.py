b267271d4153d604f3b9cc417256171ab519a125
Tomek Główka <tk.glowka@gmail.com>
2015-12-25 17:21:03 +0100

    update_graphql_schema command generating schema.json

---
+++ djrere/frontpage/management/commands/update_graphql_schema.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+
+from django.core.management.base import BaseCommand
+from graphql.core.utils.introspection_query import introspection_query
+
+from djrere.frontpage.schema import schema
+
+
+class Command(BaseCommand):
+    help = ''
+    args = ''
+    can_import_settings = True
+
+    def handle(self, *args, **options):
+        from django.conf import settings
+
+        json_schema = json.dumps({'data': schema.execute(introspection_query).data})
+        filepath = os.path.join(getattr(settings, 'BASE_PATH', './'), 'schema.json')
+        with open(filepath, mode='wb') as f:
+            f.write(json_schema)
