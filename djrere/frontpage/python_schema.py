a0ab3856264ec9384512ad656a95759d1c9ad62c
Tomek Główka <tk.glowka@gmail.com>
2016-01-14 00:13:52 +0100

    GraphQL schema rewritten, added DjangoNode schema.

--- djrere/frontpage/python_schema.py
+++ djrere/frontpage/python_schema.py
@@ -1,41 +1,46 @@
 import graphene
 from django.utils.functional import SimpleLazyObject
 from graphene import relay
-from graphene.contrib.django import DjangoNode
 
 from . import models
 
 
-class Comment(DjangoNode):
+class Comment(relay.Node):
     link = graphene.Field('FrontLink')
     content = graphene.String()
 
-    class Meta:
-        model = models.Comment
-
     def resolve_content(self, args, info):
-        return self.instance.content
+        return self._root.content
 
     def resolve_link(self, args, info):
-        return self.instance.link
+        obj = self._root.link
+        return FrontLink(id=obj.pk, _root=obj)
+
+    @classmethod
+    def get_node(cls, local_id, info=None):
+        obj = models.Comment.objects.get(pk=local_id)
+        return cls(id=obj.pk, _root=obj)
 
 
-class FrontLink(DjangoNode):
+class FrontLink(relay.Node):
     href = graphene.String()
     description = graphene.String()
     comments = relay.ConnectionField(Comment)
 
-    class Meta:
-        model = models.FrontLink
-
     def resolve_href(self, args, info):
-        return self.instance.href
+        return self._root.href
 
     def resolve_description(self, args, info):
-        return self.instance.description
+        return self._root.description
 
     def resolve_comments(self, args, info):
-        return self.instance.comments.all()
+        return [Comment(id=obj.pk, _root=obj) for obj in self._root.comments.all()]
+
+    @classmethod
+    def get_node(cls, local_id, info=None):
+        print 'get_node'
+        obj = models.FrontLink.objects.get(pk=local_id)
+        return cls(id=obj.pk, _root=obj)
 
 
 class Query(graphene.ObjectType):
@@ -46,9 +51,9 @@ class Query(graphene.ObjectType):
     all_comments = relay.ConnectionField(Comment)
 
     def resolve_all_front_links(self, args, info):
-        return models.FrontLink.objects.all()
+        return [FrontLink(id=obj.pk, _root=obj) for obj in models.FrontLink.objects.all()]
 
     def resolve_all_comments(self, args, info):
-        return models.Comment.objects.all()
+        return [Comment(id=obj.pk, _root=obj) for obj in models.Comment.objects.all()]
 
 local_schema = SimpleLazyObject(lambda: graphene.Schema(query=Query))
