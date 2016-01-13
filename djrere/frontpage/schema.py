ec2c922d213f26dec1d4733b9578ecbb8a2c312e
Tomek Główka <tk.glowka@gmail.com>
2016-01-13 01:08:59 +0100

    Real schema resolves using django ORM

--- djrere/frontpage/schema.py
+++ djrere/frontpage/schema.py
@@ -1,34 +1,41 @@
 import graphene
 from django.utils.functional import SimpleLazyObject
 from graphene import relay
-from graphql_relay import from_global_id
 
 from . import models
 
 
-def get_node(global_id):
-    node_class, local_id = from_global_id(global_id)
-    node_class(id=global_id)
+class AwareEdge(relay.Edge):
+    def resolve_node(self, args, info):
+        return self.node_type(self.node)
 
 
 class AwareNode(relay.Node):
     class Meta:
         abstract = True
 
-    @classmethod
-    def get_node(cls, local_id=1, info=None):
-        return cls(id=local_id)
+    def __init__(self, root):
+        super(AwareNode, self).__init__()
+        self.root = root
+        self.id = self.root.pk
 
+    edge_type = AwareEdge
+    
 
 class Comment(AwareNode):
     link = graphene.Field('FrontLink')
     content = graphene.String()
 
     def resolve_content(self, args, info):
-        return getattr(self, 'content', 'content_value')
+        return self.root.content
 
     def resolve_link(self, args, info):
-        return getattr(self, 'link', FrontLink.get_node())
+        return self.root.link
+
+    @classmethod
+    def get_node(cls, local_id, info=None):
+        return Comment.objects.get(pk=local_id)
+
 
 
 class FrontLink(AwareNode):
@@ -37,13 +44,19 @@ class FrontLink(AwareNode):
     comments = relay.ConnectionField(Comment)
 
     def resolve_href(self, args, info):
-        return getattr(self, 'href', 'href_value')
+        return self.root.href
 
     def resolve_description(self, args, info):
-        return getattr(self, 'desc', 'desc_value')
+        return self.root.description
 
     def resolve_comments(self, args, info):
-        return [Comment.get_node()]
+        return self.root.comments.all()
+
+    @classmethod
+    def get_node(cls, local_id, info=None):
+        return FrontLink.objects.get(pk=local_id)
+
+
 
 
 class Query(graphene.ObjectType):
